845a14a9275101afc7876b2ecc61a9eb46108a4b
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-09 15:56:48 +0600

    add: ourwork section design and responsiveness

--- src/components/OurWork.tsx
+++ src/components/OurWork.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const OurWork = () => {
     return (
-        <section className='w-11/12 mx-auto mt-10'>
-            <section className='flex justify-center items-center'>
-        	    <h1>Our Work</h1>
+        <section className='w-11/12 mx-auto mt-20'>
+            <section className='flex flex-col justify-center items-center'>
+                <h1 className='font-poppins text-5xl font-semibold mt-4 text-center'>Our Work</h1>
+                <div className="flex gap-5 mt-3">
+                        <img src='/icons/line.svg' className='w-[85px]'/>
+                        <img src='/icons/mustache.svg'/>
+                        <img src='/icons/line.svg' className='w-[85px]'/>
+                    </div>  
             </section>
-            <section className='grid grid-cols-3 gap-5 mt-5'>
+            <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-11'>
                 <figure><img src='/work1.jpg'/></figure>
                 <figure><img src='/work2.jpg'/></figure>
                 <figure><img src='/work3.jpg'/></figure>
