108be2a662a2f9d150878bd4cfd783a395ad642a
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-09 17:19:20 +0600

    add: footer design and reponsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,69 +2,62 @@ import React from 'react';
 
 const Footer = () => {
     return (
-     <footer className="bg-[url('/footer.jpg')]">
-           <footer className="footer text-white  p-10 mt-10 ">
+     <footer className="bg-[url('/footer.jpg')] font-poppins pb-10">
+           <footer className="footer text-white  p-10 mt-10 grid  lg:grid-cols-4 gap-10">
   <section className='flex flex-col items-center'>
     <img src='/title.svg'/>
-  <p className='max-w-[400px] text-center'>There are many variations of passages of lorem ipsum available, but the majority have suffered alteration in some form.</p>
-  <input type='email'/>
-  <button className='p-2 rounded-lg text-white bg-[#D9A536D4]'>Subscribe</button>
+  <p className='max-w-[300px] font-poppins text-center'>There are many variations of passages of lorem ipsum available, but the majority have suffered alteration in some form.</p>
+  <input type='email' placeholder='Email*' className='mt-10 w-full text-lg text-black p-2 px-4'/>
+  <button className='py-1 text-lg mt-2 uppercase px-4 text-white bg-[#D9A536D4] rounded-2xl border-2 border-white'>Subscribe</button>
   </section>
-  <nav className='uppercase'>
-    <h6 className="footer-title">Quick links</h6>
-    <a className="link link-hover">Home</a>
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Gallery</a>
-    <a className="link link-hover">Contact Us</a>
+  <nav className='uppercase flex flex-col '>
+                <h1 className='font-poppins text-2xl font-semibold mt-4'>Get In Touch</h1>
+                <div className="flex gap-2 mt-3">
+                        <img src='/icons/line.svg' className='w-[50px]'/>
+                        <img src='/icons/mustache.svg' className='w-[50px]'/>
+                        <img src='/icons/line.svg' className='w-[50px]'/>
+                    </div>  
+    <a className="link link-hover flex gap-3 items-center text-xl uppercase"><img src='/icons/bullet.svg'/> Home</a>
+    <a className="link link-hover flex gap-3 items-center text-xl uppercase"><img src='/icons/bullet.svg'/> About Us</a>
+    <a className="link link-hover flex gap-3 items-center text-xl uppercase"><img src='/icons/bullet.svg'/> Gallery</a>
+    <a className="link link-hover flex gap-3 items-center text-xl uppercase"><img src='/icons/bullet.svg'/> Contact US</a>
+
   </nav>
-  <nav className='uppercase'>
-    <h6 className="footer-title">Services</h6>
-    <a className="link link-hover">Hair</a>
-    <a className="link link-hover">Beauty</a>
-    <a className="link link-hover">SPA</a>
-    <a className="link link-hover">MASSAGE</a>
+  <nav className='uppercase flex flex-col '>
+                <h1 className='font-poppins text-2xl font-semibold mt-4'>Get In Touch</h1>
+                <div className="flex gap-2 mt-3">
+                        <img src='/icons/line.svg' className='w-[50px]'/>
+                        <img src='/icons/mustache.svg' className='w-[50px]'/>
+                        <img src='/icons/line.svg' className='w-[50px]'/>
+                    </div>  
+    <a className="link link-hover flex gap-3 items-center text-xl uppercase"><img src='/icons/bullet.svg'/> Hairs</a>
+    <a className="link link-hover flex gap-3 items-center text-xl uppercase"><img src='/icons/bullet.svg'/> Beauty</a>
+    <a className="link link-hover flex gap-3 items-center text-xl uppercase"><img src='/icons/bullet.svg'/> Spa</a>
+    <a className="link link-hover flex gap-3 items-center text-xl uppercase"><img src='/icons/bullet.svg'/> Massage</a>
+
   </nav>
-  <nav>
-    <h6 className="footer-title">Social</h6>
-    <div className="grid grid-flow-col gap-4">
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-        </svg>
-      </a>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-        </svg>
-      </a>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          className="fill-current">
-          <path
-            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-        </svg>
-      </a>
+  <nav className='uppercase flex flex-col gap-4 '>
+                <div className='w-fit md:mx-auto'>
+                <h1 className='w-full font-poppins text-2xl font-semibold mt-4'>Contact</h1>
+                <div className="flex gap-2 mt-3">
+                        <img src='/icons/line.svg' className='w-[50px]'/>
+                        <img src='/icons/mustache.svg' className='w-[50px]'/>
+                        <img src='/icons/line.svg' className='w-[50px]'/>
+                    </div> </div> 
+    <a className="link link-hover flex gap-3 items-center text-lg uppercase"><img src='/icons/tele.svg'/> +4125987645</a>
+    <a className="link link-hover flex gap-3 items-center text-lg uppercase"><img src='/icons/message.svg'/> barberz.123@info.com</a>
+    <a className="link link-hover flex gap-3 items-center text-lg uppercase"><img src='/icons/gps.svg'/> golden street, chinatown
+    san  francisco</a>
+    <div className='flex gap-5 md:justify-between w-full mt-5'>
+      <img src='/icons/facebook.svg'/>
+      <img src='/icons/twitter.svg'/>
+      <img src='/icons/youtube.svg'/>
+      <img src='/icons/insta.svg'/>
     </div>
   </nav>
 
 </footer>
-<h6 className='text-center text-white'>Copyright 2024 All Rights Reserved</h6>
+<h6 className='text-center text-white mt-2'>Copyright 2024 All Rights Reserved</h6>
      </footer>
     );
 };
