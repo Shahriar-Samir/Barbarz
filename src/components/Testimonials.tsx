b557a0fd6860266530338e4481468db886f2db49
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-09 14:25:07 +0600

    add: testimonials section design and responsiveness

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,23 +2,30 @@ import React from 'react';
 
 const Testimonials = () => {
     return (
-        <section className='bg-[url("/review.jpg")] h-[100vh] bg-cover flex justify-center items-center flex-col'>
+        <section className='bg-[url("/review.jpg")] bg-cover flex justify-center items-center flex-col gap-28 py-24'>
              <section className='flex flex-col items-center gap-5'>
-            <h2 className='text-[#CCA34C] text-'>Testimonials</h2>
-                    <h1 className=' text-white font-semibold'>Hear From Our Customers</h1>
-                    <div className='divide-x-0'>
-                    mustache
+             <h3 className='text-[#CCA34C] text-4xl'>Specialists</h3>
+                    <h1 className='text-white text-5xl font-semibold font-poppins mt-3'>Our Team</h1>
+                    <div className="flex gap-5 mt-3">
+                        <img src='/icons/line.svg'/>
+                        <img src='/icons/mustache.svg'/>
+                        <img src='/icons/line.svg'/>
                     </div>
             </section>
-            <section className='flex gap-5 w-11/12 mx-auto'>
-                <article className='bg-[#252424] text-white p-5'>
-                    <div>
-                    <img src='/icons/testimonial1.svg'/>
-                    <p>“Amazing service! Claire helped me to reduce the shipping price a little and shipped it immediately after purchasing. The amethyst cave I got is a beauty and I c...”</p>
-                    <div className='flex justify-between w-full'>
-                <div>
-                <h1>Robert Fox</h1>
-                    <h6>Writer</h6>
+            <section className='grid grid-cols-1 md:grid-cols-2 gap-7 md:gap-5 w-11/12 mx-auto'>
+                
+                <article className='bg-[#252424] text-white p-5 flex gap-5 flex-col lg:flex-row'>
+                <figure className='w-[150px] h-[150px] lg:w-[350px] lg:h-[250px]'>
+                <img src='/icons/testimonial1.svg' className='w-full h-fit object-cover'/>
+                </figure>
+                    <div className='h-full flex flex-col justify-between gap-12'>
+                    <p className="text-xl font-medium font-poppins">“Amazing service! Claire helped me to reduce the shipping price a little and shipped it immediately after purchasing. The amethyst cave I got is a beauty and I c...”</p>
+                <div className=' flex justify-between w-full'>
+                <div className='w-full flex flex-col gap-3'>
+                <h1 className='text-4xl font-medium text-[#B78E36] font-poppins'>Robert Fox</h1>
+                   <div className='w-full flex justify-between'>
+                  <div className='flex flex-col gap-2'>
+                  <h6 className=''>Writer</h6>
                     <div className="rating">
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
   <input
@@ -29,19 +36,27 @@ const Testimonials = () => {
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked/>
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" disabled/>
 </div>
+                  </div>
+<img src='/icons/quote.svg'/>
+                   </div>
                 </div>
-                <img src='/icons/quote.svg'/>
-                    </div>
+           
+                </div>
+
                     </div>
                 </article>
-                <article className='bg-[#252424] text-white p-5'>
-                    <div>
-                    <img src='/icons/testimonial2.svg'/>
-                    <p>“Amazing service! Claire helped me to reduce the shipping price a little and shipped it immediately after purchasing. The amethyst cave I got is a beauty and I c...”</p>
-                    <div className='flex justify-between w-full'>
-                <div>
-                <h1>Robert Fox</h1>
-                    <h6>Writer</h6>
+                <article className='bg-[#252424] text-white p-5 flex flex-col lg:flex-row gap-5'>
+                 <figure className='w-[150px] h-[150px] lg:w-[350px] lg:h-[250px]'>
+                <img src='/icons/testimonial2.svg' className='w-full h-fit object-cover'/>
+                </figure>
+                    <div className='h-full flex flex-col justify-between gap-12'>
+                    <p className="text-xl font-medium font-poppins">“Just came back to home and should say that it is definitely a great experience. I would recommend it for everyone who needs a bike for a short term or want to t...”</p>
+                <div className=' flex justify-between w-full'>
+                <div className='w-full flex flex-col gap-3'>
+                <h1 className='text-4xl font-medium text-[#B78E36] font-poppins'>Marvin McKinney</h1>
+                   <div className='w-full flex justify-between'>
+                  <div className='flex flex-col gap-2'>
+                  <h6 className=''>Businessman</h6>
                     <div className="rating">
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
   <input
@@ -52,11 +67,16 @@ const Testimonials = () => {
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked/>
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" disabled/>
 </div>
+                  </div>
+<img src='/icons/quote.svg'/>
+                   </div>
                 </div>
-                <img src='/icons/quote.svg'/>
-                    </div>
+           
+                </div>
+
                     </div>
                 </article>
+                
             </section>
         </section>
     );
