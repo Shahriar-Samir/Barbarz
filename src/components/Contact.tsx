792eaa8b0be8656e16b6bef9bcd805aa9f682684
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-09 12:24:36 +0600

    add: reponsiveness on contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <section className='w-full flex gap-5 items-center bg-[url("/contact.jpg")]'>
-            <section className='flex flex-col justify-center px-10  w-4/6 text-white h-[700px]'>
+        <section className='w-full flex flex-col md:flex-row gap-5 items-center bg-[url("/contact.jpg")] pb-10 md:pb-0'>
+            <section className='flex flex-col justify-center px-10 items-center md:items-start  md:w-4/6 text-white h-[700px]'>
                 <h3 className='text-4xl text-[#CCA34C]'>Contact</h3>
                 <h1 className='font-poppins text-5xl font-semibold mt-4'>Get In Touch</h1>
                 <div className="flex gap-5 mt-3">
@@ -11,10 +11,10 @@ const Contact = () => {
                         <img src='/icons/mustache.svg'/>
                         <img src='/icons/line.svg' className='w-[85px]'/>
                     </div>  
-                <p className='w-[550px] text-lg mt-4'>Suspendisse pretium felis dolor, sed placerat diam blandit vitae. Sed eu nisl massa. Phasellus vehicula tristique orci ut ultrices. Duis eu libero augue. In vulputate sodales tempus. Phasellus dolor orci, consectetur in tincidunt eget, consectetur non orci. </p>
+                <p className='w-11/12 max-w-[550px] text-lg mt-4 text-center md:text-start'>Suspendisse pretium felis dolor, sed placerat diam blandit vitae. Sed eu nisl massa. Phasellus vehicula tristique orci ut ultrices. Duis eu libero augue. In vulputate sodales tempus. Phasellus dolor orci, consectetur in tincidunt eget, consectetur non orci. </p>
             </section>
-            <section className='flex flex-col items-center justify-center w-3/6 h-[700px] px-3 bg-[#292727B3] text-white'>
-                <h1 className='text-4xl text-[#CCA34C] font-semibold font-poppins'>Make An Appointment</h1>
+            <section className='flex flex-col items-center justify-center md:w-3/6 h-[700px] px-3 bg-[#292727B3] text-white w-11/12 '>
+                <h1 className='text-4xl text-center text-[#CCA34C] font-semibold font-poppins'>Make An Appointment</h1>
                 <p className='capitalize text-center font-poppins mt-2'>Barber is a person whose occupation is mainly to cut dress groom</p>
                 <form className='mt-12'>
                     <section className='grid grid-cols-2 gap-5'>
@@ -51,7 +51,7 @@ const Contact = () => {
 
                     </section>
                     <div className="form-control mt-6">
-                    <button className='mt-6 btn bg-[#CCA34C] text-white rounded-none uppercase p-0 px-14 min-h-10 h-16 text-2xl hover:bg-[#a3833d]'>Make An Appointment</button>
+                    <button className='mt-6 btn bg-[#CCA34C] text-white rounded-none uppercase p-0 px-14 min-h-10 h-16 text-md lg:text-2xl hover:bg-[#a3833d]'>Make An Appointment</button>
         </div>
                 </form>
             </section>
